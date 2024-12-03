e43733e52396e6c90f66838d5d157d0c58bee0d6
A98603 <A98603@ucu.ac.ug>
2024-12-03 10:41:16 +0300

    changes made by A98603

--- automation.py
+++ automation.py
@@ -1,1 +1,9 @@
+import os
 
+def check_file(/home/automation.py):
+    if os.path.isfile(/home/ubuntu/class.txt):
+        print(f"The file '{/home/class.txt}' exists.")
+    else:
+        print(f"The file '{/home/class.txt}' does not exist.")
+
+#this code checks if the file class.txt exist in that given directory`
